d5d08397ef0441455258477a99f66f61aa11ca25
Vishal Reddy <vreddy18@gmail.com>
2014-01-04 18:08:24 -0500

    Submitting movie app

---
+++ __vishal_reddy/movie_app_copy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieApp::Application.config.secret_key_base = '5c0aec84703c85c44358a86b041c4d9f68867a8adc83510a12c9517837bae7895e8ff73d48490b7b5ea3d3569ff28dd51c70ed5d5ce5acce47681c60f647b46b'
